7e43a59511b61dc42a2f9b66731cb6c6e252c2ca
Ahmed faysal <ahmedfoysal01797@gmail.com>
2024-01-08 13:39:24 +0600

    bg update

--- chefs-client/src/pages/menuPage/Menu.jsx
+++ chefs-client/src/pages/menuPage/Menu.jsx
@@ -86,9 +86,7 @@ const Menu = () => {
   return (
     <div>
       {/* menu banner */}
-      <div
-        className={`max-w-screen-2xl container mx-auto xl:px-24 px-4 bg-gradient-to-r from-0% from-[#FAFAFA] to-[#FCFCFC] to-100%`}
-      >
+      <div className={`max-w-screen-2xl container mx-auto xl:px-24 px-4`}>
         <div className="py-48 flex flex-col items-center justify-center">
           {/* content */}
           <div className=" text-center px-4 space-y-7">
